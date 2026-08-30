e3c9eeccbde55f9873d65b874432e15c17ba018a
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2026-08-30 23:36:02 +0530

    Auto-deploy update with pre-built dist 2026-08-30 23:36:02

--- backend/splitwise_backend/settings.py
+++ backend/splitwise_backend/settings.py
@@ -13,6 +13,41 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# Full CORS & CSRF permissiveness for cross-origin local/production access
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://13.235.142.203:5001",
+    "http://13.235.142.203:5002",
+    "http://localhost:5001",
+    "http://localhost:5002",
+    "http://localhost:5173",
+    "http://localhost:8000",
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -80,8 +115,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
